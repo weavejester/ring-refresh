6d1f744f7d7f2830b7b048c75ce73074789748cd
James Reeves <jreeves@weavejester.com>
2012-05-13 21:24:00 +0100

    Middleware to add blank script tag to HTML responses

--- src/ring/middleware/refresh.clj
+++ src/ring/middleware/refresh.clj
@@ -1,1 +1,24 @@
-(ns ring.middleware.refresh)
+(ns ring.middleware.refresh
+  (:require [clojure.string :as str]))
+
+(defn- success? [response]
+  (<= 200 (:status response) 299))
+
+(defn- html-content? [response]
+  (re-find
+   #"text/html"
+   (get-in response [:headers "Content-Type"])))
+
+(defn add-refresh-script [body]
+  (str/replace
+   body
+   #"<head\s*[^>]*>"
+   #(str % "<script></script>")))
+
+(defn wrap-refresh
+  [handler]
+  (fn [request]
+    (let [response (handler request)]
+      (if (and (success? response) (html-content? response))
+        (update-in response [:body] add-refresh-script)
+        response))))
