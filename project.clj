d22dadd5fcd5cefc21fd81ed2649ed637b329807
James Reeves <jreeves@weavejester.com>
2012-05-19 23:19:12 +0100

    URL and description added to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject ring-refresh "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Ring middleware for automatically refreshing HTML pages"
+  :url "https://github.com/weavejester/ring-refresh"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
