a10c5f38dadde7b3a03078f77957a90844ed7f58
James Reeves <jreeves@weavejester.com>
2013-01-20 16:21:33 +0000

    Released 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-refresh "0.1.1"
+(defproject ring-refresh "0.1.2"
   :description "Ring middleware for automatically refreshing HTML pages"
   :url "https://github.com/weavejester/ring-refresh"
   :license {:name "Eclipse Public License"
